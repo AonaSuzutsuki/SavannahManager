4e22f06e10a9fc724d85c0e1b5fc9e79f80c1735
涼月蒼菜 <rvvfake.org@gmail.com>
2017-08-08 21:54:12 +0900

    共通ライブラリの拡張メソッドの名前空間を変更

--- CommonStyleLib/Extentions/Extentions.cs
+++ CommonStyleLib/Extentions/Extentions.cs
@@ -5,22 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommonLib.Models
+namespace CommonLib.Extentions
 {
-    public static class Extentions
+    public static class DictionaryExtentions
     {
         public static void ForEach<TKey, TValue>(this Dictionary<TKey, TValue> dic, Action<TKey, TValue> act)
         {
             foreach (KeyValuePair<TKey, TValue> val in dic)
                 act?.Invoke(val.Key, val.Value);
         }
-
+    }
+    public static class ListExtentions
+    {
         public static void ForEachInIndex<TValue>(this List<TValue> list, Action<int, TValue> act)
         {
             foreach (var item in list.Select((v, i) => new { v, i }))
                 act?.Invoke(item.i, item.v);
         }
+    }
 
+    public static class ObservableCollectionExtentions
+    {
         public static void AddAll<TValue>(this ObservableCollection<TValue> collection, List<TValue> list)
         {
             list.ForEach((val) => collection.Add(val));
