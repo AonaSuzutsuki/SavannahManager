d6848f124821c35cbb78bce504ac0a28cd8f8d87
BuildTools <unconfigured@null.spigotmc.org>
2018-11-25 02:25:02 +0900

    Provisional support to a17.

--- 7dtd-svmanager-fix-mvvm/Models/ServerProcessManager.cs
+++ 7dtd-svmanager-fix-mvvm/Models/ServerProcessManager.cs
@@ -14,16 +14,19 @@ namespace _7dtd_svmanager_fix_mvvm.Models
         private Process p = new Process();
         public ServerProcessManager(string exeFilePath, string configFilePath)
         {
+            configFilePath = Path.GetFileName(configFilePath);
             p.StartInfo = new ProcessStartInfo() {
                 FileName = exeFilePath,
-                Arguments = string.Format("-quit -batchmode -nographics -configfile=\"{0}\" -dedicated", configFilePath),
+                Arguments = string.Format("-logfile 7DaysToDieServer_Data\\output_log.txt -quit -batchmode -nographics -configfile={0} -dedicated", configFilePath),
                 UseShellExecute = false,
                 CreateNoWindow = false,
                 RedirectStandardInput = false,
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
-                WorkingDirectory = Path.GetDirectoryName(exeFilePath)
+                WorkingDirectory = Path.GetDirectoryName(configFilePath)
             };
+            p.StartInfo.EnvironmentVariables["SteamAppId"] = "251570";
+            p.StartInfo.EnvironmentVariables["SteamGameId"] = "251570";
         }
 
         public bool ProcessStart(Action<string> failedAction = null)
