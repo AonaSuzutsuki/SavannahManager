6ce670f694aa0722bbcff5779b19bc494ce530d9
AonaSuzutsuki <rvvfake.org@gmail.com>
2019-09-15 16:25:21 +0900

    support fileviewer and restoring.

--- 7dtd-svmanager-fix-mvvm/Backup/Models/BackupSelectorModel.cs
+++ 7dtd-svmanager-fix-mvvm/Backup/Models/BackupSelectorModel.cs
@@ -2,24 +2,93 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using _7dtd_svmanager_fix_mvvm.Backup.Models.Image;
 using BackupLib.Backup;
+using BackupLib.CommonPath;
 using CommonCoreLib.CommonPath;
+using CommonExtensionLib.Extensions;
+using CommonStyleLib.ExMessageBox;
 using CommonStyleLib.Models;
 
 namespace _7dtd_svmanager_fix_mvvm.Backup.Models
 {
-    public class BackupItem
+    public class BackupItem : PathMapItem
     {
-        public string Text { get; set; }
+        public ImageSource ImageSource { get; set; }
+
+        public string DateString
+        {
+            get
+            {
+                if (Date != DateTime.MinValue)
+                    return Date.ToString("yyyy/MM/dd");
+                return string.Empty;
+            }
+        }
+
+        public static IEnumerable<BackupItem> ToBackupItem(IEnumerable<PathMapItem> pathMapItems)
+        {
+            var list = new List<BackupItem>();
+            foreach (var pathMapItem in pathMapItems)
+            {
+                ImageSource imageSource = ImageLoader.LoadFromResource("_7dtd_svmanager_fix_mvvm.Backup.Images.no-image.png");
+                if (pathMapItem.ItemType == PathMapItemType.File)
+                    imageSource = ImageLoader.LoadFromResource("_7dtd_svmanager_fix_mvvm.Backup.Images.FileIcon.png");
+                if (pathMapItem.ItemType == PathMapItemType.Directory)
+                    imageSource = ImageLoader.LoadFromResource("_7dtd_svmanager_fix_mvvm.Backup.Images.DirectoryIcon.png");
+
+                list.Add(new BackupItem
+                {
+                    ImageSource = imageSource,
+                    Name = pathMapItem.Name,
+                    Parent = pathMapItem.Parent,
+                    FullPath = pathMapItem.FullPath,
+                    ItemType = pathMapItem.ItemType,
+                    Files = pathMapItem.Files,
+                    Date = pathMapItem.Date,
+                });
+            }
+
+            return list;
+        }
     }
 
     public class BackupSelectorModel : ModelBase
     {
+        #region Properties
         public ObservableCollection<string> BackupList { get; set; } = new ObservableCollection<string>();
-        public ObservableCollection<BackupItem> BackupFileList { get; set; }
+        public ObservableCollection<BackupItem> BackupFileList { get; set; } = new ObservableCollection<BackupItem>();
+
+        public bool CanRestore
+        {
+            get => canRestore;
+            set => SetProperty(ref canRestore, value);
+        }
+
+        public bool ForwardBtIsEnabled
+        {
+            get => forwardBtIsEnabled;
+            set => SetProperty(ref forwardBtIsEnabled, value);
+        }
+
+        public bool BackBtIsEnabled
+        {
+            get => backBtIsEnabled;
+            set => SetProperty(ref backBtIsEnabled, value);
+        }
+
+        public string PathText
+        {
+            get => pathText;
+            set => SetProperty(ref pathText, value);
+        }
 
         public int BackupProgressValue
         {
@@ -32,28 +101,34 @@ namespace _7dtd_svmanager_fix_mvvm.Backup.Models
             get => progressLabel;
             set => SetProperty(ref progressLabel, value);
         }
+        #endregion
 
+        #region Fields
 
+        private string sevenDaysSavePath;
+
+        private bool canRestore;
+        private bool forwardBtIsEnabled;
+        private bool backBtIsEnabled;
+        private string pathText;
         private int backupProgressValue;
         private string progressLabel;
 
-        private readonly TimeMachine timeMachine;
+        private Stack<PathMapItem> forwardStack = new Stack<PathMapItem>();
+        private PathMapItem current;
+        #endregion
+
+        private readonly TimeBackup timeBackup;
 
         public BackupSelectorModel()
         {
-            BackupFileList = new ObservableCollection<BackupItem>
-            {
-                new BackupItem { Text = "1"},
-                new BackupItem { Text = "2"},
-                new BackupItem { Text = "3"}
-            };
-
-            var userDir = System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            timeMachine = new TimeMachine("backup", $"{userDir}/7DaysToDie");
-            timeMachine.BackupCompleted += TimeMachineOnBackupCompleted;
-            timeMachine.BackupProgress += TimeMachineOnBackupProgress;
-            timeMachine.BackupStarted += TimeMachineOnBackupStarted;
-            timeMachine.RestoreProgress += TimeMachineOnRestoreProgress;
+            var userDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            sevenDaysSavePath = $"{userDir}/7DaysToDie";
+            timeBackup = new TimeBackup("backup", sevenDaysSavePath);
+            timeBackup.BackupCompleted += TimeBackupOnBackupCompleted;
+            timeBackup.BackupProgress += TimeBackupOnBackupProgress;
+            timeBackup.BackupStarted += TimeBackupOnBackupStarted;
+            timeBackup.RestoreProgress += TimeBackupOnRestoreProgress;
 
             InitializeBackupList();
         }
@@ -61,53 +136,125 @@ namespace _7dtd_svmanager_fix_mvvm.Backup.Models
         private void InitializeBackupList()
         {
             BackupList.Clear();
-            var trace = timeMachine.TraceBackup();
+            var trace = timeBackup.TraceBackup();
             foreach (var backupDate in trace)
             {
                 BackupList.Add(backupDate);
             }
         }
 
-        private void TimeMachineOnRestoreProgress(object sender, TimeMachine.BackupProgressEventArgs eventArgs)
+        private void TimeBackupOnRestoreProgress(object sender, TimeBackup.BackupProgressEventArgs eventArgs)
         {
-
+            BackupProgressValue = eventArgs.Percentage;
         }
 
-        private void TimeMachineOnBackupStarted(object sender, TimeMachine.BackupCompletedEventArgs eventArgs)
+        private void TimeBackupOnBackupStarted(object sender, TimeBackup.BackupCompletedEventArgs eventArgs)
         {
             switch (eventArgs.CompletedMode)
             {
-                case TimeMachine.Mode.Backup:
+                case TimeBackup.Mode.Restore:
+                    ProgressLabel = "Restore...";
+                    break;
+                case TimeBackup.Mode.Backup:
                     ProgressLabel = "Backup...";
                     break;
-                case TimeMachine.Mode.ComputeHash:
+                case TimeBackup.Mode.ComputeHash:
                     ProgressLabel = "Analyze...";
                     break;
             }
         }
 
-        private void TimeMachineOnBackupProgress(object sender, TimeMachine.BackupProgressEventArgs eventArgs)
+        private void TimeBackupOnBackupProgress(object sender, TimeBackup.BackupProgressEventArgs eventArgs)
         {
             BackupProgressValue = eventArgs.Percentage;
         }
 
-        private void TimeMachineOnBackupCompleted(object sender, TimeMachine.BackupCompletedEventArgs eventArgs)
+        private void TimeBackupOnBackupCompleted(object sender, TimeBackup.BackupCompletedEventArgs eventArgs)
         {
             switch (eventArgs.CompletedMode)
             {
-                case TimeMachine.Mode.NothingToBackup:
+                case TimeBackup.Mode.NothingToBackup:
                     ProgressLabel = "Nothing to backup.";
                     break;
-                case TimeMachine.Mode.Backup:
+                case TimeBackup.Mode.Backup:
                     ProgressLabel = "Completed to backup.";
+                    break;
+                case TimeBackup.Mode.Restore:
+                    ProgressLabel = "Completed to restore.";
                     break;
             }
         }
 
+        public void Restore()
+        {
+            if (!timeBackup.CanRestore)
+                return;
+
+            var result = ExMessageBoxBase.Show("Are you sure to start restoring?", "Start to restore",
+                ExMessageBoxBase.MessageType.Exclamation, ExMessageBoxBase.ButtonType.YesNo);
+            if (result == ExMessageBoxBase.DialogResult.Yes)
+                Task.Factory.StartNew(() => timeBackup.Restore());
+        }
+
         public void Backup()
         {
-            Task.Factory.StartNew(timeMachine.Backup).ContinueWith((t) => InitializeBackupList());
+            Task.Factory.StartNew(timeBackup.Backup).ContinueWith((t) => InitializeBackupList());
+        }
 
+        public void SelectBackup(int index)
+        {
+            if (index > -1)
+            {
+                timeBackup.SelectBackup(index);
+                forwardStack = new Stack<PathMapItem>();
+                ForwardBtIsEnabled = forwardStack.Count > 0;
+            }
+            CanRestore = timeBackup.CanRestore;
+        }
+
+        public void NewDirectoryChange()
+        {
+            forwardStack = new Stack<PathMapItem>();
+            ForwardBtIsEnabled = false;
+        }
+
+        public void DirectoryForward()
+        {
+            var pathMapItem = forwardStack.Pop();
+            DirectoryChange(pathMapItem);
+            ForwardBtIsEnabled = forwardStack.Count > 0;
+        }
+        
+        public void DirectoryBack()
+        {
+            if (current?.Parent != null)
+            {
+                forwardStack.Push(current);
+                DirectoryChange(current.Parent);
+            }
+            ForwardBtIsEnabled = forwardStack.Count > 0;
+        }
+
+        public void DrawBackup()
+        {
+            var root = timeBackup.GetFileAndDirectories();
+            if (root != null && root.Files.Count > 0)
+                DirectoryChange(root);
+        }
+
+        public void DirectoryChange(PathMapItem pathMapItem)
+        {
+            if (pathMapItem != null &&
+                (pathMapItem.ItemType == PathMapItem.PathMapItemType.Directory || pathMapItem.ItemType == PathMapItem.PathMapItemType.Root))
+            {
+                BackupFileList.Clear();
+                if (pathMapItem.Files.Count > 0)
+                    BackupFileList.AddAll(BackupItem.ToBackupItem(pathMapItem.Files));
+
+                current = pathMapItem;
+                BackBtIsEnabled = pathMapItem.Parent != null;
+                PathText = pathMapItem.ToString();
+            }
         }
     }
 }
--- 7dtd-svmanager-fix-mvvm/Backup/ViewModels/BackupSelectorViewModel.cs
+++ 7dtd-svmanager-fix-mvvm/Backup/ViewModels/BackupSelectorViewModel.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using _7dtd_svmanager_fix_mvvm.Backup.Models;
+using BackupLib.CommonPath;
 using CommonStyleLib.Models;
 using CommonStyleLib.ViewModels;
 using Prism.Commands;
@@ -26,13 +27,24 @@ namespace _7dtd_svmanager_fix_mvvm.Backup.ViewModels
             BackupFileList = model.BackupFileList.ToReadOnlyReactiveCollection();
             BackupProgressValue = model.ObserveProperty(m => m.BackupProgressValue).ToReactiveProperty();
             ProgressLabel = model.ObserveProperty(m => m.ProgressLabel).ToReactiveProperty();
+            RestoreBtEnabled = model.ObserveProperty(m => m.CanRestore).ToReactiveProperty();
+            ForwardBtIsEnabled = model.ObserveProperty(m => m.ForwardBtIsEnabled).ToReactiveProperty();
+            BackBtIsEnabled = model.ObserveProperty(m => m.BackBtIsEnabled).ToReactiveProperty();
+            PathText = model.ToReactivePropertyAsSynchronized(m => m.PathText);
+            DeleteBtEnabled = model.ObserveProperty(m => m.CanRestore).ToReactiveProperty();
 
+            RestoreBtClicked = new DelegateCommand(RestoreBt_Clicked);
             BackupBtClicked = new DelegateCommand(BackupBt_Clicked);
+            DeleteBtClicked = new DelegateCommand(DeleteBt_Clicked);
+            BackupListSelectionChanged = new DelegateCommand<int?>(BackupList_SelectionChanged);
+            ForwardBtClicked = new DelegateCommand(ForwardBt_Clicked);
+            BackBtClicked = new DelegateCommand(BackBt_Clicked);
+            BackupFileListMouseDoubleClick = new DelegateCommand<PathMapItem>(BackupFileList_MouseDoubleClick);
         }
 
         #region Fields
 
-        private BackupSelectorModel model;
+        private readonly BackupSelectorModel model;
 
         #endregion
 
@@ -41,22 +53,72 @@ namespace _7dtd_svmanager_fix_mvvm.Backup.ViewModels
         public ReadOnlyReactiveCollection<string> BackupList { get; set; }
         public ReadOnlyReactiveCollection<BackupItem> BackupFileList { get; set; }
 
+        public ReactiveProperty<bool> RestoreBtEnabled { get; set; }
+        public ReactiveProperty<bool> ForwardBtIsEnabled { get; set; }
+        public ReactiveProperty<bool> BackBtIsEnabled { get; set; }
+        public ReactiveProperty<string> PathText { get; set; }
         public ReactiveProperty<int> BackupProgressValue { get; set; }
         public ReactiveProperty<string> ProgressLabel { get; set; }
+
+        public ReactiveProperty<bool> DeleteBtEnabled { get; set; }
 
         #endregion
 
         #region Event Properties
 
+        public ICommand RestoreBtClicked { get; set; }
         public ICommand BackupBtClicked { get; set; }
+        public ICommand DeleteBtClicked { get; set; }
+
+        public ICommand BackupListSelectionChanged { get; set; }
+
+        public ICommand ForwardBtClicked { get; set; }
+        public ICommand BackBtClicked { get; set; }
+        public ICommand BackupFileListMouseDoubleClick { get; set; }
+
 
         #endregion
 
         #region Event Methods
 
+        public void RestoreBt_Clicked()
+        {
+            model.Restore();
+        }
         public void BackupBt_Clicked()
         {
             model.Backup();
+        }
+
+        public void DeleteBt_Clicked()
+        {
+
+        }
+
+        public void BackupList_SelectionChanged(int? index)
+        {
+            if (index == null)
+                return;
+
+            var value = index.Value;
+            model.SelectBackup(value);
+            model.DrawBackup();
+        }
+
+        public void ForwardBt_Clicked()
+        {
+            model.DirectoryForward();
+        }
+
+        public void BackBt_Clicked()
+        {
+            model.DirectoryBack();
+        }
+
+        public void BackupFileList_MouseDoubleClick(PathMapItem pathMapItem)
+        {
+            model.NewDirectoryChange();
+            model.DirectoryChange(pathMapItem);
         }
 
         #endregion
