818f500a255b1fbe9ecf46e355aa0779ab9f3192
涼月蒼菜 <rvvfake.org@gmail.com>
2019-11-07 17:22:40 +0900

    added 7dtd xml editor.

---
+++ 7dtd-ConfigEditorCUI/XmlWrapper/CommonXmlReader.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using CommonCoreLib.CommonLinq;
+using CommonExtensionLib.Extensions;
+
+namespace CommonCoreLib.XMLWrapper
+{
+    /// <summary>
+    /// Read from a file as XML Document.
+    /// </summary>
+    public class CommonXmlReader
+    {
+        public string XmlPath { get; } = string.Empty;
+
+        private readonly XmlDocument document = new XmlDocument();
+
+        public CommonXmlReader(string xmlPath, bool isFile = true)
+        {
+            if (isFile)
+            {
+                XmlPath = xmlPath;
+                document.Load(xmlPath);
+            }
+            else
+            {
+                document.LoadXml(xmlPath);
+            }
+        }
+
+        public CommonXmlReader(Stream stream)
+        {
+            document.Load(stream);
+        }
+
+        public List<string> GetAttributes(string attribute, string xpath, bool isContainNoValue = false)
+        {
+            var nodeList = document.SelectNodes(xpath).ToList();
+            if (isContainNoValue)
+            {
+                return (from node in nodeList
+                       let attr = (node as XmlElement).GetAttribute(attribute)
+                       select attr).ToList();
+            }
+
+            return (from node in nodeList
+                    let attr = (node as XmlElement).GetAttribute(attribute)
+                    where !string.IsNullOrEmpty(attr)
+                    select attr).ToList();
+        }
+        public string GetAttribute(string attribute, string xpath)
+        {
+            var attributes = GetAttributes(attribute, xpath);
+            return attributes.Count < 1 ? string.Empty : attributes[0];
+        }
+
+        public List<string> GetValues(string xpath, bool enableLineBreak = true, bool isRemoveSpace = true)
+        {
+            var nodeList = document.SelectNodes(xpath).ToList();
+
+            return (from node in nodeList
+                    let text = (node as XmlElement).InnerText
+                    let value = Conditions.IfElse(isRemoveSpace, () => RemoveSpace(text, enableLineBreak), () => text)
+                    select value).ToList();
+        }
+        public string GetValue(string xpath, bool enableLineBreak = true, bool isRemoveSpace = true)
+        {
+            var values = GetValues(xpath, enableLineBreak, isRemoveSpace);
+            return values.Count < 1 ? default : values[0];
+        }
+
+        private static string RemoveSpace(string text, bool isAddLine = false)
+        {
+            var sb = new StringBuilder();
+
+            text = text.Replace("\r\n", "\r").Replace("\r", "\n").TrimStart('\n');
+            var spaceLength = GetSpaceLength(text);
+
+            var expression = spaceLength > 0 ? $"^( {{0,{spaceLength.ToString()}}})(?<text>.*)$" : "^ *(?<text>.*)$";
+            var reg = new Regex(expression);
+            using var sr = new StringReader(text);
+            while (sr.Peek() > -1)
+            {
+                var line = sr.ReadLine() ?? string.Empty;
+
+                var match = reg.Match(line);
+                if (match.Success)
+                    if (isAddLine)
+                        sb.Append($"{match.Groups["text"].Value}\n");
+                    else
+                        sb.Append(match.Groups["text"].Value);
+                else
+                    sb.Append(sr.ReadLine());
+            }
+            return sb.ToString().TrimStart('\n').TrimEnd('\n');
+        }
+
+        private static int GetSpaceLength(string text)
+        {
+            const string expression = "^(?<space>[\\s\\t]*)(?<text>.*)$";
+            var reg = new Regex(expression);
+            var textArray = text.Split('\n');
+            if (textArray.Length > 0)
+            {
+                var match = reg.Match(textArray[0]);
+                if (match.Success)
+                    return match.Groups["space"].Value.Length;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 7dtd-ConfigEditorCUI/XmlWrapper/CommonXmlWriter.cs
@@ -0,0 +1,136 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace CommonCoreLib.XMLWrapper
+{
+    public class CommonXmlElement
+    {
+        private readonly XmlElement element;
+
+        public CommonXmlElement(XmlElement elem)
+        {
+            element = elem;
+        }
+
+        public void Append(CommonXmlElement elem)
+        {
+            element.AppendChild(elem.element);
+        }
+
+        public void Append(params CommonXmlElement[] elements)
+        {
+            foreach (var elem in elements)
+                Append(elem);
+        }
+
+        public void AppendToXmlDocument(XmlDocument xmlDocument)
+        {
+            xmlDocument.AppendChild(element);
+        }
+    }
+
+    /// <summary>
+    /// Write to a file as XML Document.
+    /// </summary>
+    public class CommonXmlWriter
+    {
+        private XmlDocument xDocument = new XmlDocument();
+        private XmlProcessingInstruction xDeclaration;
+
+        public CommonXmlWriter()
+        {
+            xDeclaration = xDocument.CreateProcessingInstruction("xml", "version=\"1.0\"");
+        }
+        
+        /// <summary>
+        /// Set a root.
+        /// </summary>
+        /// <param name="rootName">Root name</param>
+        public CommonXmlElement CreateRoot(string rootName)
+        {
+            return new CommonXmlElement(xDocument.CreateElement(rootName));
+        }
+
+        /// <summary>
+        /// Add an element.
+        /// </summary>
+        /// <param name="elementName">Element name.</param>
+        /// <param name="attributeInfos">Attribute informations.</param>
+        /// <param name="value">Attribute value.</param>
+        public CommonXmlElement CreateElement(string elementName, AttributeInfo[] attributeInfos, string value = null)
+        {
+            XmlElement xmeta = xDocument.CreateElement(elementName);
+            foreach (AttributeInfo attributeInfo in attributeInfos)
+                xmeta.SetAttribute(attributeInfo.Name, attributeInfo.Value);
+            if (!string.IsNullOrEmpty(value))
+                xmeta.InnerText = value;
+
+            return new CommonXmlElement(xmeta);
+        }
+
+        /// <summary>
+        /// Add an element.
+        /// </summary>
+        /// <param name="elementName">Element name.</param>
+        /// <param name="attributeInfo">Attribute information.</param>
+        /// <param name="value">Attribute value.</param>
+        public CommonXmlElement CreateElement(string elementName, AttributeInfo attributeInfo, string value = null)
+        {
+            XmlElement xmeta = xDocument.CreateElement(elementName);
+            xmeta.SetAttribute(attributeInfo.Name, attributeInfo.Value);
+            if (!string.IsNullOrEmpty(value))
+                xmeta.InnerText = value;
+
+            return new CommonXmlElement(xmeta);
+        }
+        public CommonXmlElement CreateElement(string elementName, string value = null)
+        {
+            XmlElement xmeta = xDocument.CreateElement(elementName);
+            if (!string.IsNullOrEmpty(value))
+                xmeta.InnerText = value;
+
+            return new CommonXmlElement(xmeta);
+        }
+
+        public CommonXmlElement CreateElement(string elementName, AttributeInfo attributeInfo, CommonXmlElement value)
+        {
+            XmlElement xmeta = xDocument.CreateElement(elementName);
+            xmeta.SetAttribute(attributeInfo.Name, attributeInfo.Value);
+
+            var commonXmlElem = new CommonXmlElement(xmeta);
+            commonXmlElem.Append(value);
+
+            return commonXmlElem;
+        }
+
+        public CommonXmlElement CreateElement(string elementName, CommonXmlElement value)
+        {
+            XmlElement xmeta = xDocument.CreateElement(elementName);
+
+            var commonXmlElem = new CommonXmlElement(xmeta);
+            commonXmlElem.Append(value);
+
+            return commonXmlElem;
+        }
+
+        /// <summary>
+        /// Write to a file as XML Dcoument.
+        /// </summary>
+        public void Write(string xmlPath, CommonXmlElement root)
+        {
+            var fs = new FileStream(xmlPath, FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
+            Write(fs, root);
+            fs.Dispose();
+        }
+        /// <summary>
+        /// Write to a file as XML Dcoument.
+        /// </summary>
+        public void Write(Stream stream, CommonXmlElement root)
+        {
+            xDocument.AppendChild(xDeclaration);
+            root.AppendToXmlDocument(xDocument);
+            
+            xDocument.Save(stream);
+        }
+    }
+}
