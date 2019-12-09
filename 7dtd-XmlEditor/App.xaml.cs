1a4b87b7b579c258c1d99370313ad42c798bd7cd
AonaSuzutsuki <rvvfake.org@gmail.com>
2019-12-09 10:34:03 +0900

    fixed exception code.

--- 7dtd-XmlEditor/App.xaml.cs
+++ 7dtd-XmlEditor/App.xaml.cs
@@ -12,28 +12,16 @@ using CommonCoreLib;
 
 namespace _7dtd_XmlEditor
 {
-    /// <summary>
-    /// App.xaml の相互作用ロジック
-    /// </summary>
-    public partial class App : Application
+    public class ExceptionWriter
     {
-        private void MyApp_Startup(object sender, StartupEventArgs e)
-        {
-            var mainWindow = new MainWindow();
-            mainWindow.Show();
-        }
-
-        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        public static void WriteAndShowLog(Exception exception)
         {
             var mes = string.Format("予期せぬエラーが発生しました。\r\nお手数ですが、開発者に例外内容を報告してください。\r\n\r\n---\r\n\r\n{0}\r\n\r\n{1}",
-                e.Exception.Message, e.Exception.StackTrace);
+                exception.Message, exception.StackTrace);
             MessageBox.Show(mes, "予期せぬエラー", MessageBoxButton.OK, MessageBoxImage.Error);
 
             var dt = DateTime.Now;
             OutToFile(AppInfo.GetAppPath() + @"\error-" + dt.ToString("yyyy-MM-dd- HH-mm-ss") + ".log", mes);
-
-            e.Handled = true;
-            Shutdown();
         }
 
         private static void OutToFile(string filename, string text)
@@ -41,6 +29,41 @@ namespace _7dtd_XmlEditor
             using var fs = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
             using var sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
             sw.Write(text);
+        }
+}
+
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void MyApp_Startup(object sender, StartupEventArgs e)
+        {
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+            //AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception exception)
+                ExceptionWriter.WriteAndShowLog(exception.InnerException);
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            ExceptionWriter.WriteAndShowLog(e.Exception.InnerException);
+            e.SetObserved();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            ExceptionWriter.WriteAndShowLog(e.Exception);
+
+            e.Handled = true;
+            Shutdown();
         }
     }
 }
