762c081abf6d75aeb8172a51c7379499acb35b31
涼月蒼菜 <rvvfake.org@gmail.com>
2020-11-11 17:01:34 +0900

    added steam id.

---
+++ SvManagerLibrary/Steam/SteamIdValidator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace SvManagerLibrary.Steam
+{
+    public class SteamIdValidator
+    {
+        public enum Universe : ulong
+        {
+            Individual,
+            Public,
+            Beta,
+            Internal,
+            Dev,
+            RC,
+        }
+
+        public enum AccountType : ulong
+        {
+            Invalid,
+            Individual,
+            Multiseat,
+            GameServer,
+            AnonGameServer,
+            Pending,
+            ContentServer,
+            Clan,
+            Chat,
+            P2PSuperSeeder,
+            AnonUser
+        }
+
+        public const ulong UniverseMask = 0xFF00000000000000;
+        public const ulong TypeMask = 0x00F0000000000000;
+        public const ulong InstanceMask = 0x000FFFFF00000000;
+        public const ulong AccountNumberMask = 0x00000000FFFFFFFE;
+        public const ulong LsbMask = 0x0000000000000001;
+
+        public static bool ValidateSteamId(ulong steamId)
+        {
+            var universe = (steamId & UniverseMask) >> 56;
+            var type = (steamId & TypeMask) >> 52;
+            //var instance = (steamId & InstanceMask) >> 32;
+            //var accountNumber = (steamId & AccountNumberMask) >> 1;
+            //var lsb = steamId & LsbMask;
+
+            if (!Enum.IsDefined(typeof(Universe), universe))
+                return false;
+            if (!Enum.IsDefined(typeof(AccountType), type))
+                return false;
+            return true;
+        }
+    }
+}
---
+++ SvManagerLibraryTests2/Steam/SteamIdValidatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using SvManagerLibrary.Steam;
+
+namespace SvManagerLibraryTests2.Steam
+{
+    [TestFixture]
+    public class SteamIdValidatorTest
+    {
+        [Test]
+        public void ValidateSteamIdTest()
+        {
+            var validator = new SteamIdValidator();
+            var isValidated = SteamIdValidator.ValidateSteamId(76561198010715714);
+            var isValidated2 = SteamIdValidator.ValidateSteamId(413435657435212879042);
+
+            Assert.AreEqual(true, isValidated);
+            Assert.AreEqual(false, isValidated2);
+        }
+    }
+}
