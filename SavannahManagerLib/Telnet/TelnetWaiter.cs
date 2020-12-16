a481c47286badb1a110a010462fa53664fff41ba
涼月蒼菜 <rvvfake.org@gmail.com>
2020-12-16 16:50:57 +0900

    added comments on telnet and steam library.

--- SavannahManagerLib/Telnet/TelnetWaiter.cs
+++ SavannahManagerLib/Telnet/TelnetWaiter.cs
@@ -2,17 +2,36 @@
 
 namespace SvManagerLibrary.Telnet
 {
+    /// <summary>
+    /// Calculates the Telnet wait time.
+    /// </summary>
     public class TelnetWaiter
     {
         private int _count;
 
-        public bool CanLoop => Count < MaxMilliseconds;
+        /// <summary>
+        /// Checks if it is loopable.
+        /// </summary>
+        public bool CanLoop => ElapsedSeconds < MaxMilliseconds;
+
+        /// <summary>
+        /// Gets or sets the maximum wait time in milliseconds.
+        /// </summary>
         public int MaxMilliseconds { get; set; } = 2000;
 
-        public int Count => _count * SleepTime;
+        /// <summary>
+        /// Get the elapsed time for executing Next method.
+        /// </summary>
+        public int ElapsedSeconds => _count * SleepTime;
 
+        /// <summary>
+        /// Gets or sets the time to sleep the thread in milliseconds.
+        /// </summary>
         public int SleepTime { get; set; } = 100;
 
+        /// <summary>
+        /// Only SleepTime will sleep and proceed to the next.
+        /// </summary>
         public void Next()
         {
             _count++;
