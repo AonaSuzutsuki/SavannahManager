d51f58446a80a6384c10305e31d5f295df56fd75
AonaSuzutsuki <rvvfake.org@gmail.com>
2021-10-14 23:29:36 +0900

    fixed build

--- build.ps1
+++ build.ps1
@@ -6,7 +6,7 @@ if ( [string]::IsNullOrEmpty($outName) ) {
 
 $CurrentDirectory = Split-Path $MyInvocation.MyCommand.Path -Parent
 $OutBinDirectory = "$CurrentDirectory\$outName"
-$Framework = "net48"
+$Framework = "netcoreapp3.1"
 
 $SavannahManagerReleaseDirectory = "$CurrentDirectory\SavannahManager\bin\Release\$Framework"
 $XmlEditorReleaseDirectory = "$CurrentDirectory\SavannahXmlEditor\bin\Release\$Framework"
@@ -15,24 +15,28 @@ $UpdaterReleaseDirectory = "$CurrentDirectory\Updater\bin\Release\$Framework"
 
 #SavannahManager
 xcopy /Y $SavannahManagerReleaseDirectory\*.dll $OutBinDirectory\
-xcopy /Y $SavannahManagerReleaseDirectory\*.config $OutBinDirectory\
 xcopy /Y $SavannahManagerReleaseDirectory\*.exe $OutBinDirectory\
+xcopy /Y $SavannahManagerReleaseDirectory\*.deps.json $OutBinDirectory\
+xcopy /Y $SavannahManagerReleaseDirectory\*.runtimeconfig.json $OutBinDirectory\
 Copy-Item -Path $SavannahManagerReleaseDirectory\Settings\ -Destination "$OutBinDirectory\Settings" -Recurse -Force
 Copy-Item -Path $SavannahManagerReleaseDirectory\en-US\ -Destination "$OutBinDirectory\en-US" -Recurse -Force
 
 # XmlEditor
 xcopy /Y $XmlEditorReleaseDirectory\*.dll "$OutBinDirectory\XmlEditor\"
-xcopy /Y $XmlEditorReleaseDirectory\*.config "$OutBinDirectory\XmlEditor\"
 xcopy /Y $XmlEditorReleaseDirectory\*.exe "$OutBinDirectory\XmlEditor\"
+xcopy /Y $XmlEditorReleaseDirectory\*.deps.json "$OutBinDirectory\XmlEditor\"
+xcopy /Y $XmlEditorReleaseDirectory\*.runtimeconfig.json "$OutBinDirectory\XmlEditor\"
 
 # ConfigEditor
 xcopy /Y $ConfigEditorReleaseDirectory\*.dll "$OutBinDirectory\ConfigEditor\"
-xcopy /Y $ConfigEditorReleaseDirectory\*.config "$OutBinDirectory\ConfigEditor\"
 xcopy /Y $ConfigEditorReleaseDirectory\*.exe "$OutBinDirectory\ConfigEditor\"
+xcopy /Y $ConfigEditorReleaseDirectory\*.deps.json "$OutBinDirectory\ConfigEditor\"
+xcopy /Y $ConfigEditorReleaseDirectory\*.runtimeconfig.json "$OutBinDirectory\ConfigEditor\"
 Copy-Item -Path $ConfigEditorReleaseDirectory\lang\ -Destination "$OutBinDirectory\ConfigEditor\" -Recurse -Force
 Copy-Item -Path $ConfigEditorReleaseDirectory\en-US\ -Destination "$OutBinDirectory\ConfigEditor\" -Recurse -Force
 
 # Updater
 xcopy /Y $UpdaterReleaseDirectory\*.dll "$OutBinDirectory\Updater\"
-xcopy /Y $UpdaterReleaseDirectory\*.config "$OutBinDirectory\Updater\"
 xcopy /Y $UpdaterReleaseDirectory\*.exe "$OutBinDirectory\Updater\"
+xcopy /Y $UpdaterReleaseDirectory\*.deps.json "$OutBinDirectory\Updater\"
+xcopy /Y $UpdaterReleaseDirectory\*.runtimeconfig.json "$OutBinDirectory\Updater\"
