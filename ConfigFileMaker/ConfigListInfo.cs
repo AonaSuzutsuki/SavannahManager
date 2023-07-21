f9682f18fbd6f8fab2d2c01d24c5eb96c43d1dfc
AonaSuzutsuki <rvvfake.org@gmail.com>
2023-07-21 15:08:25 +0900

    fixed config file maker.

---
+++ ConfigFileMaker/ConfigListInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConfigFileMaker
+{
+    /// <summary>
+    /// Information for Confit List.
+    /// </summary>
+    public class ConfigListInfo
+    {
+        public string Property { get; set; }
+        public string Value { get; set; }
+        public string Selection { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
---
+++ ConfigFileMaker/ConfigType.cs
@@ -0,0 +1,12 @@
+﻿namespace ConfigFileMaker
+{
+    /// <summary>
+    /// Type for config value.
+    /// </summary>
+    public enum ConfigType
+    {
+        String,
+        Integer,
+        Combo
+    }
+}
