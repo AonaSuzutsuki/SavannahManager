e071f3b36cf543b0e5e3bb5a2c3db2626969bc24
AonaSuzutsuki <rvvfake.org@gmail.com>
2025-02-13 16:17:01 +0900

    updated packages.

--- SavannahManagerLib/Chat/ChatInfoConverter.cs
+++ SavannahManagerLib/Chat/ChatInfoConverter.cs
@@ -17,6 +17,9 @@ namespace SvManagerLibrary.Chat
         {
             //2019-01-19T16:14:21 140.048 INF Chat (from '-non-player-', entity id '-1', to 'Global'): 'Server': test
             // "^(?<date>.*?) (.*?) INF Chat: '(?<name>.*?)': (?<chat>.*?)$";
+
+            // 2024-07-21T15:44:58 26527.982 INF Chat (from 'Steam_76561198010715714', entity id '171', to 'Global'): kusa
+            // 2024-07-17T16:56:21 7943.935 INF Chat (from '-non-player-', entity id '-1', to 'Global'): gomi
             const string expression = "^(?<date>[0-9a-zA-Z:-]+) ([0-9.]+?) INF Chat \\(from '(?<steamId>[a-zA-Z0-9_-]+)', entity id '(?<id>[0-9-]+)'.*\\): '(?<name>.*)': (?<chat>.*)$";
             var reg = new Regex(expression);
             var sr = new StringReader(text);
